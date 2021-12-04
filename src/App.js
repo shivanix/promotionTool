628d9e10a410d176bdf4f4f5b95dae85bc0c6072
Shivani <shvni.here@gmail.com>
2021-12-04 18:38:02 -0500

    Add classNames for popup divs; Add image to show in popup

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     const branches = [
         {branchName: "BranchOne", latitude: 43.87310, longitude: -79.28572, id: "b1", image:"https://images.unsplash.com/photo-1614656048454-dda85616f0a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=80&q=100"},
         {branchName: "BranchTwo", latitude: 43.88319, longitude: -79.30572, id: "b2", image: "https://images.unsplash.com/photo-1550827783-07a572d03390?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDR8fGJhbmslMjBtYXJrZXRpbmd8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"},
-        {branchName: "BranchThree", latitude: 44.88319, longitude: -80.30572, id: "b3", image: "https://images.unsplash.com/photo-1518534543674-5933a2307dca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" }
+        {branchName: "BranchThree", latitude: 43.86319, longitude: -78.30572, id: "b3", image: "https://images.unsplash.com/photo-1518534543674-5933a2307dca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" }
 
     ]
 
@@ -64,7 +64,11 @@ function App() {
                     })}
 
                     {selectedBranch ? (<Popup longitude={selectedBranch.longitude} latitude={selectedBranch.latitude}>
+                            <div className="popup-img">
+                                <img src={selectedBranch.image}/> </div>
+                            <div className="popup-text">
                             <h2>{selectedBranch.branchName}</h2>
+                            </div>
                         </Popup>
                     ) : null}
                 </ReactMapGL>
