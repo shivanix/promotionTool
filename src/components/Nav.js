5b2428c9f93fc3847d8e075c9e245e0818f99311
Shivani <shvni.here@gmail.com>
2021-12-04 21:52:28 -0500

    Add Nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+
+export default function Nav() {
+    return(
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Marketer</li>
+                <li>End-User</li>
+            </ul>
+        </nav>
+    )
+}
