c476f6aa0f89aa596932812f781125d6990c6c22
Shivani <shvni.here@gmail.com>
2021-12-04 20:16:35 -0500

    Modify data info of branches; display info in popup

--- src/App.js
+++ src/App.js
@@ -7,9 +7,27 @@ import BranchList from "./components/BranchList";
 function App() {
 
     const branches = [
-        {branchName: "BranchOne", latitude: 43.87310, longitude: -79.28572, id: "b1", image:"https://images.unsplash.com/photo-1614656048454-dda85616f0a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=80&q=100"},
-        {branchName: "BranchTwo", latitude: 43.88319, longitude: -79.30572, id: "b2", image: "https://images.unsplash.com/photo-1550827783-07a572d03390?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDR8fGJhbmslMjBtYXJrZXRpbmd8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"},
-        {branchName: "BranchThree", latitude: 43.86319, longitude: -78.30572, id: "b3", image: "https://images.unsplash.com/photo-1518534543674-5933a2307dca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" }
+        {id: "b1",
+            branchName: "BranchOne",
+            latitude: 43.87310,
+            longitude: -79.28572,
+            offer:"Offer 1",
+            image: "https://images.unsplash.com/photo-1614656048454-dda85616f0a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=80&q=100"
+        },
+        {id: "b2",
+            branchName: "BranchTwo",
+            latitude: 43.88319,
+            longitude: -79.30572,
+            offer:"Offer 2",
+            image: "https://images.unsplash.com/photo-1550827783-07a572d03390?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDR8fGJhbmslMjBtYXJrZXRpbmd8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+        },
+        {id: "b3",
+            branchName: "BranchThree",
+            latitude: 43.86319,
+            longitude: -78.30572,
+            offer:"Offer 3",
+            image: "https://images.unsplash.com/photo-1518534543674-5933a2307dca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+        }
 
     ]
 
@@ -66,13 +84,14 @@ function App() {
                     {selectedBranch ? (<div className="popup-main"><Popup
                             longitude={selectedBranch.longitude}
                             latitude={selectedBranch.latitude}
-                            onClose={()=>{
+                            onClose={() => {
                                 setSelectedBranch(null)
                             }}>
                             <div className="popup-img">
-                                <img src={selectedBranch.image}/> </div>
+                                <img src={selectedBranch.image} alt="thumbnail"/></div>
                             <div className="popup-text">
-                            <h2>{selectedBranch.branchName}</h2>
+                                <h2>{selectedBranch.branchName}</h2>
+                                <p>{selectedBranch.offer}</p>
                             </div>
                         </Popup>
                         </div>
