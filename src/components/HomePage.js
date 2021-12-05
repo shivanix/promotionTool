b840c2d4f7cb976b9c0be0461974948ccd25d867
Shivani <shvni.here@gmail.com>
2021-12-04 22:03:29 -0500

    Add HomePage comp

---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import Button from "./Button";
+
+export default function HomePage() {
+    return(
+        <div>
+            <div>
+                <Button>MArketer</Button>
+            </div>
+            <div>
+                <Button>EndUSrer</Button>
+            </div>
+        </div>
+    )
+}
