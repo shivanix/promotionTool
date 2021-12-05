ce0d86c84cafd1ec7f13c0686f6a5c9844a33189
Shivani <shvni.here@gmail.com>
2021-12-04 21:53:09 -0500

    Add EndUser comp

---
+++ src/components/EndUser.js
@@ -0,0 +1,11 @@
+
+
+export default function EndUser() {
+return(
+    <form>
+        <label>Latitude</label>
+        <input type="text"/>
+        <button type="submit">Add Coordinates</button>
+    </form>
+);
+}
