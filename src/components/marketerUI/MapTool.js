abb29eb3d296a0bf758722cd4eb0e102e0a22944
Shivani <shvni.here@gmail.com>
2021-12-05 19:10:50 -0500

    Import mapboxgl; implement functionality to display a map

--- src/components/marketerUI/MapTool.js
+++ src/components/marketerUI/MapTool.js
@@ -1,78 +1,39 @@
-import ReactMapGL, {GeolocateControl, Marker, Popup, StaticMap} from "react-map-gl";
-import {useState} from "react";
+// import ReactMapGL, {GeolocateControl, Marker, Popup, StaticMap} from "react-map-gl";
+import {useEffect, useRef, useState} from "react";
+import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
 export default function MapTool(props) {
 
+    mapboxgl.accessToken = "pk.eyJ1Ijoic2hpdmFuaXgiLCJhIjoiY2t3cmExaHZyMHVxODMxbnljMWhhdzF3eiJ9.P9Fsyeu_1o61PHKGsTa96g";
 
-    const [viewport, setViewport] = useState({
-        latitude: 43.89310,
-        longitude: -79.29572,
-        zoom: 10,
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-79.29);
+    const [lat, setLat] = useState(43.893);
+    const [zoom, setZoom] = useState(9);
 
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [lng, lat],
+            zoom: zoom
+        });
     });
+    // const [viewport, setViewport] = useState({
+    //     latitude: 43.89310,
+    //     longitude: -79.29572,
+    //     zoom: 10,
+    //
+    // });
 
     const [selectedBranch, setSelectedBranch] = useState(null)
 
 
     return (
         <div className="second-container">
-            <ReactMapGL
-                {...viewport}
-                onViewportChange={nextViewport => setViewport(nextViewport)}
-                mapboxApiAccessToken="pk.eyJ1Ijoic2hpdmFuaXgiLCJhIjoiY2t3cmExaHZyMHVxODMxbnljMWhhdzF3eiJ9.P9Fsyeu_1o61PHKGsTa96g"
-                mapStyle="mapbox://styles/shivanix/ckwrgbosw16pq14odakx3r50k"
-                width="70vw"
-                height="100vh"
-
-            >
-                {props.branchesDetails.map((location) => {
-                    return <Marker key={location.id}
-                                   latitude={location.latitude}
-                                   longitude={location.longitude}>
-                        <button className="marker-btn"
-                                onMouseOver={(e) => {
-                                    e.preventDefault();
-                                    setSelectedBranch(location)
-
-                                }}
-                        >
-                            <img src="/mapbox-marker-icon-orange.svg" alt=" "/>
-                        </button>
-                    </Marker>
-                })}
-
-                {selectedBranch ? (<div className="popup-main"><Popup
-                        longitude={selectedBranch.longitude}
-                        latitude={selectedBranch.latitude}
-                        onClose={() => {
-                            setSelectedBranch(null)
-                        }}>
-                        <div className="popup-img">
-                            <img src={selectedBranch.image} alt="thumbnail"/></div>
-                        <div className="popup-text">
-                            <h2>{selectedBranch.branchName}</h2>
-                            <p>{selectedBranch.offer}</p>
-                        </div>
-                    </Popup>
-                    </div>
-                ) : null}
-                {/*{console.log("viewwwwww", viewport)}*/}
-                <GeolocateControl
-                    positionOptions={{enableHighAccuracy: true}}
-                    trackUserLocation={true}
-                    showUserLocation={true}
-
-
-                    onGeolocate={('click', (e) => {
-                        let cordino = e
-                        console.log("Did you find it", cordino)
-
-                    })}
-                />
-
-
-
-            </ReactMapGL>
+            <div ref={mapContainer} className="map-container" />
         </div>
     )
 }
