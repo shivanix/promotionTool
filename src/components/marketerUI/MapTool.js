d29b38a213039ee0119ad11a272541cc8682f2bf
Shivani <shvni.here@gmail.com>
2021-12-05 22:08:27 -0500

    Add functionality to log coords when clicked on map

--- src/components/marketerUI/MapTool.js
+++ src/components/marketerUI/MapTool.js
@@ -2,7 +2,6 @@ import {useEffect, useRef, useState} from "react";
 import mapboxgl from 'mapbox-gl';
 
 
-
 export default function MapTool(props) {
 
     mapboxgl.accessToken = "pk.eyJ1Ijoic2hpdmFuaXgiLCJhIjoiY2t3cmExaHZyMHVxODMxbnljMWhhdzF3eiJ9.P9Fsyeu_1o61PHKGsTa96g";
@@ -22,11 +21,11 @@ export default function MapTool(props) {
             zoom: zoom
         });
 
-        // Create a default Markers and add it to the map.
-        props.branchesDetails.map((eachBranch)=>{
-            new mapboxgl.Marker()
-                .setLngLat([eachBranch.longitude,eachBranch.latitude])
-                .setPopup(new mapboxgl.Popup({offset: 30}).setHTML(`<div class="popup-img"><img src=${eachBranch.image} alt="thumbnail"/></div>
+        // Create default Markers and popups and add it to the map.
+        props.branchesDetails.map((eachBranch) => {
+            new mapboxgl.Marker({color: 'orange'})
+                .setLngLat([eachBranch.longitude, eachBranch.latitude])
+                .setPopup(new mapboxgl.Popup({offset: 30 }).setHTML(`<div class="popup-img"><img src=${eachBranch.image} alt="thumbnail"/></div>
             <div class="popup-text">
                 <h2>${eachBranch.branchName}</h2>
                 <p>${eachBranch.offer}</p>
@@ -35,10 +34,17 @@ export default function MapTool(props) {
                 .addTo(map.current)
         })
 
+        const marker = new mapboxgl.Marker();
+        function add_marker (event) {
+            const coordinates = event.lngLat;
+            console.log('Lng:', coordinates.lng, 'Lat:', coordinates.lat);
+            marker.setLngLat(coordinates).addTo(map.current);
+        }
+
+        map.current.on('click', add_marker);
     });
 
     const [selectedBranch, setSelectedBranch] = useState(null)
-
 
 
     return (
