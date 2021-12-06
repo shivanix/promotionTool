51342271897d9ba1777f73e4862a017f2786b5a1
Shivani <shvni.here@gmail.com>
2021-12-06 04:01:53 -0500

    Add containers; give classnames to form elements

--- src/components/AddBranch.js
+++ src/components/AddBranch.js
@@ -1,14 +1,13 @@
 import {useState} from "react";
-
+import "./addBranch.css"
 export default function AddBranch() {
 
     const [name, setName] = useState('');
     const [offer, setOffer] = useState('');
     const [imgurl, setImgurl] = useState('');
 
-    
-
-
+    let branches = {'name': 'newName', 'image': 'thumbnail', 'latitude': 43.87310,
+        'longitude': -79.28572, 'offer': 'offer' }
 
     const handle = () => {
         localStorage.setItem('Name', name);
@@ -16,10 +15,23 @@ export default function AddBranch() {
         localStorage.setItem('Img-url', imgurl);
     };
 
+
+    const updateData= (data) => {
+
+    }
+        // localStorage.setItem('Branches', branches object})
+        // newBranch = {....new branch data}
+        //
+        // let updatedBranches = localStorage.getItem('Branches')
+        //
+        // updatedBranches[newBranch.id] = newBranch
+        //
+        // localStorage.setItem('Branches', updatedBranches})
+
     return (
         <form>
-            <div>
-                <div>
+            <div className="new-branch-controls">
+                <div className="new-branch--control">
                     <label >New Branch Name</label>
                     <input type="text" id="bName" name="newBranchname" placeholder="New Branch.."
                     value={name}
@@ -27,7 +39,7 @@ export default function AddBranch() {
 
                     />
                 </div>
-                <div>
+                <div className= "new-branch--control">
                     <label >Promotional Offer</label>
                     <input type="text" id="oName" name="newOffer" placeholder="New promotional offer.."
                     value={offer}
@@ -35,13 +47,15 @@ export default function AddBranch() {
                     />
                 </div>
 
-                <div>
+                <div className= "new-branch--control">
                     <label >Image URL</label>
                     <input type="text" id="img-url" name="image-url" placeholder="Add Image URL.."
                     value={imgurl}
                            onChange={(e)=>{setImgurl(e.target.value)}}
                     />
                 </div>
+            </div>
+                <div className="new-branch-submit">
                 <button onClick={handle}>Submit</button>
             </div>
 
