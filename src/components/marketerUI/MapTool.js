e6687761f6798e64943a7e93ae4e82dbdfd83a13
Shivani <shvni.here@gmail.com>
2021-12-06 03:06:14 -0500

    Add new methods for map functionality

--- src/components/marketerUI/MapTool.js
+++ src/components/marketerUI/MapTool.js
@@ -12,6 +12,10 @@ export default function MapTool(props) {
     const [lat, setLat] = useState(43.893);
     const [zoom, setZoom] = useState(9);
 
+    //convert object-of-objects to an array - for mapping easily
+    const branchArr = Object.values(props.branchesDetails);
+
+
     useEffect(() => {
         if (map.current) return; // initialize map only once
         map.current = new mapboxgl.Map({
@@ -21,11 +25,28 @@ export default function MapTool(props) {
             zoom: zoom
         });
 
-        // Create default Markers and popups and add it to the map.
-        props.branchesDetails.map((eachBranch) => {
+        // Add Geolocatecontrol to the map.
+        map.current.addControl(
+            new mapboxgl.GeolocateControl({
+                positionOptions: {
+                    enableHighAccuracy: true
+                },
+// When active the map will receive updates to the device's location as it changes.
+                trackUserLocation: true,
+// Draw an arrow next to the location dot to indicate which direction the device is heading.
+                showUserHeading: false,
+// Hide accuracy circle
+                showAccuracyCircle: false,
+// Hide user location dot
+                showUserLocation: false,
+            })
+        );
+
+        // Create default Markers and popups; add it to the map.
+        branchArr.map((eachBranch) => {
             new mapboxgl.Marker({color: 'orange'})
                 .setLngLat([eachBranch.longitude, eachBranch.latitude])
-                .setPopup(new mapboxgl.Popup({offset: 30 }).setHTML(`<div class="popup-img"><img src=${eachBranch.image} alt="thumbnail"/></div>
+                .setPopup(new mapboxgl.Popup({offset: 30}).setHTML(`<div class="popup-img"><img src=${eachBranch.image} alt="thumbnail"/></div>
             <div class="popup-text">
                 <h2>${eachBranch.branchName}</h2>
                 <p>${eachBranch.offer}</p>
@@ -35,22 +56,22 @@ export default function MapTool(props) {
         })
 
         const marker = new mapboxgl.Marker();
-        function add_marker (event) {
+
+        function add_marker(event) {
             const coordinates = event.lngLat;
             console.log('Lng:', coordinates.lng, 'Lat:', coordinates.lat);
             marker.setLngLat(coordinates).addTo(map.current);
         }
 
-        map.current.on('click', add_marker);
+        // map.current.on('click', add_marker);
     });
 
-    const [selectedBranch, setSelectedBranch] = useState(null)
+    // const [selectedBranch, setSelectedBranch] = useState(null)
 
 
     return (
         <div className="second-container">
             <div ref={mapContainer} className="map-container"/>
-
         </div>
     )
 }
