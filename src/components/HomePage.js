579bf2589c5ef364dc45df88603f360c20f7d02e
Shivani <shvni.here@gmail.com>
2021-12-06 22:18:28 -0500

    Modify Home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,10 +5,17 @@ export default function HomePage() {
         <div className="home-main-container">
             <div className="btn-container">
             <div >
-                <Button className="btn"type="submit">MArketer</Button>
+                <h2>Marketer UI</h2>
+               <p> You can access the Marketer UI through the nav bar</p>
+               <p> You can add new branches by clicking on the button "Add Branch"</p>
+               <p> You can choose the location for the new branch on the map</p>
+               <p> You can fill the details of the new branch in the form generated</p>
+               <p> You can delete a branch if yu dont like it!</p>
+               <p> Deletion of a branch will remove the branch's marker as well</p>
             </div>
             <div >
-                <Button className="btn" type="submit">EndUSrer</Button>
+                <h2>End-User UI</h2>
+                <p> You can access the End-User UI through the nav bar</p>
             </div>
             </div>
         </div>
